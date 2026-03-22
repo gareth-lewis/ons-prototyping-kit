3131d0306e64838d8cc3f5abe7497ff339cda3ce
Gareth Lewis <garethlewis83@gmail.com>
2026-03-22 18:11:21 +0000

    Added README

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { default as SiteHeader } from './components/SiteHeader.astro';
+export { default as Footer } from './components/Footer.astro';
+export { default as PageBanner } from './components/PageBanner.astro';
+export { default as Heading } from './component/Heading.astro';
+export { default as TableOfContents} from './components/TableOfContents.astro';
+export { default as BaseLayout } from './layouts/BaseLayout.astro';
+export { default as TopicLayout } from './layouts/TopicLayout.astro';
+export { default as PageLayout } from './layouts/PageLayout.astro';
